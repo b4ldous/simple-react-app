6c0cc29183266a723e26d8d7074bb37f0d2481ea
b4ldous <baldous@protonmail.com>
2022-09-30 11:56:03 -0500

    url was modified in App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   );
 }
 function callApi() {
-  fetch('http://localhost:3001/details', { method: 'GET' })
+  fetch('https://simple-node-server-nw.herokuapp.com/', { method: 'GET' })
       .then(data => data.json())
       .then(json => alert(JSON.stringify(json)))
 }
